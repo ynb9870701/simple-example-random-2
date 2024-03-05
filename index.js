9fd8bb2a12fc22db81b0b45fc42832dd660bd9d2
picasso <929144387@qq.com>
2024-03-05 22:05:11 +0800

    add fn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+import dayjs from 'dayjs';
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-module.exports = random
+// 日期检测 检测当前日期是否在 date 之后
+function isAfter(currentTime, compareTime) {
+  // 使用 Day.js 解析时间
+  const current = dayjs(currentTime);
+  const compare = dayjs(compareTime);
+  
+  // 判断当前时间是否在比较时间之后
+  return current.isAfter(compare);
+}
+
+module.exports = { random, isAfter }
